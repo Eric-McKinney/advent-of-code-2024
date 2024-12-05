a23fec1556dc9d94cc35002bb0b962bfcae35b6a
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-04 23:03:16 -0500

    Add part1 solution

--- day1/Day1.java
+++ day1/Day1.java
@@ -1,6 +1,35 @@
 package aoc2024.day1;
 
 import aoc2024.AdventOfCodePuzzle;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day1 extends AdventOfCodePuzzle {
+    public Day1() {
+        this.day = 1;
+    }
+
+    @Override
+    public String part1(ArrayList<String> inputLines) {
+        ArrayList<Integer> firstList = new ArrayList<>(), secondList = new ArrayList<>();
+        int distSum = 0;
+
+        for (String line : inputLines) {
+            String[] row = line.split(" +");
+
+            firstList.add(Integer.decode(row[0]));
+            secondList.add(Integer.decode(row[1]));
+        }
+
+        Collections.sort(firstList);
+        Collections.sort(secondList);
+
+        for (int i = 0; i < firstList.size(); i++) {
+            int distance = Math.abs(firstList.get(i) - secondList.get(i));
+
+            distSum += distance;
+        }
+
+        return Integer.toString(distSum);
+    }
 }
