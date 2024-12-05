bfafe3d27465fb7ef310f8fa35f68eedc2d5bab0
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-05 00:39:37 -0500

    Add part2 solution

--- day1/Day1.java
+++ day1/Day1.java
@@ -3,6 +3,7 @@ package aoc2024.day1;
 import aoc2024.AdventOfCodePuzzle;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class Day1 extends AdventOfCodePuzzle {
     public Day1() {
@@ -31,5 +32,38 @@ public class Day1 extends AdventOfCodePuzzle {
         }
 
         return Integer.toString(distSum);
+    }
+
+    @Override
+    public String part2(ArrayList<String> inputLines) {
+        ArrayList<Integer> firstList = new ArrayList<>();
+        HashMap<Integer, Integer> secondListCounts = new HashMap<>();
+        int similaritySum = 0;
+
+        for (String line : inputLines) {
+            String[] row = line.split(" +");
+            int v1 = Integer.decode(row[0]), v2 = Integer.decode(row[1]);
+
+            firstList.add(v1);
+            
+            if (secondListCounts.containsKey(v2)) {
+                secondListCounts.put(v2, secondListCounts.get(v2) + 1);
+            } else {
+                secondListCounts.put(v2, 1);
+            }
+
+            // ensure all entries in firstList have a key in secondListCounts
+            if (!secondListCounts.containsKey(v1)) {
+                secondListCounts.put(v1, 0);
+            }
+        }
+
+        for (int i : firstList) {
+            int similarity = i * secondListCounts.get(i);
+
+            similaritySum += similarity;
+        }
+
+        return Integer.toString(similaritySum);
     }
 }
