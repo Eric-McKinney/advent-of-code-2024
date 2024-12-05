140a4402da1d57df907f8b2356a0e21e2cfdee9d
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-04 22:03:13 -0500

    Add readInput and solve

--- AdventOfCodePuzzle.java
+++ AdventOfCodePuzzle.java
@@ -1,8 +1,12 @@
+package aoc2024;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public abstract class AdventOfCodePuzzle {
-    private final String input = "input.txt";
-
     public String part1(ArrayList<String> inputLines) {
         return "";
     }
@@ -11,21 +15,32 @@ public abstract class AdventOfCodePuzzle {
         return "";
     }
 
-    public static ArrayList<String> readInput() {
-        ArrayList<String> lines = new ArrayList<>();
-
-        return lines;
-    }
-
-    public static void main(String[] args) {
-        AdventOfCodePuzzle puzzle = new AdventOfCodePuzzle();
+    public void solve() {
         ArrayList<String> inputLines = AdventOfCodePuzzle.readInput();
         String ans1, ans2;
 
-        ans1 = puzzle.part1(inputLines);
-        ans2 = puzzle.part2(inputLines);
+        ans1 = part1(inputLines);
+        ans2 = part2(inputLines);
 
         System.out.println("part 1: " + ans1);
         System.out.println("part 2: " + ans2);
+    }
+
+    public static ArrayList<String> readInput() {
+        ArrayList<String> lines = new ArrayList<>();
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader("input.txt")));
+        } catch (FileNotFoundException e) {
+            System.err.println("ERROR: Couldn't find puzzle input");
+            System.exit(1);
+        }
+
+        while (scanner.hasNext()) {
+            lines.add(scanner.nextLine());
+        }
+
+        return lines;
     }
 }
