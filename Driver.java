32325403d85ec3caf8ee475d9dba62fda3e07f31
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-07 17:19:29 -0500

    Add day3

--- Driver.java
+++ Driver.java
@@ -2,6 +2,7 @@ package aoc2024;
 
 import aoc2024.day1.Day1;
 import aoc2024.day2.Day2;
+import aoc2024.day3.Day3;
 
 public class Driver {
     public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class Driver {
 
         puzzles[0] = new Day1();
         puzzles[1] = new Day2();
+        puzzles[2] = new Day3();
 
         System.out.println(separator);
         for (int i = 0; i < puzzles.length && puzzles[i] != null; i++) {
