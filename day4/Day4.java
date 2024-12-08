49bca6b2b2cc657e8b4bdcdf372ff1c7f6f47dc0
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-07 21:28:23 -0500

    Refactor and add part2 solution

--- day4/Day4.java
+++ day4/Day4.java
@@ -2,6 +2,8 @@ package aoc2024.day4;
 
 import aoc2024.AdventOfCodePuzzle;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Day4 extends AdventOfCodePuzzle {
     public Day4() {
@@ -10,46 +12,103 @@ public class Day4 extends AdventOfCodePuzzle {
 
     @Override
     public String part1(ArrayList<String> inputLines) {
+        return runPart(inputLines, true);
+    }
+
+    @Override
+    public String part2(ArrayList<String> inputLines) {
+        return runPart(inputLines, false);
+    }
+
+    private String runPart(ArrayList<String> inputLines, boolean isPart1) {
+        XmasChecker checker = isPart1 ? new P1Checker(inputLines) : new P2Checker(inputLines);
         int qtyXmas = 0;
 
-        for (int x = 0; x < inputLines.get(0).length(); x++) {
-            for (int y = 0; y < inputLines.size(); y++) {
-                qtyXmas += checkXmas(inputLines, x, y);
+        for (int y = 0; y < inputLines.size(); y++) {
+            for (int x = 0; x < inputLines.get(y).length(); x++) {
+                qtyXmas += checker.checkXmas(x, y);
             }
         }
 
         return Integer.toString(qtyXmas);
     }
 
-    // checks coords for xmas and returns quantity found
-    private int checkXmas(ArrayList<String> wordSearch, int x, int y) {
-        int qtyXmas = 0;
-
-        for (int xdelta = -1; xdelta <= 1; xdelta++) {
-            for (int ydelta = -1; ydelta <= 1; ydelta++) {
-                qtyXmas += isXmas(wordSearch, x, y, xdelta, ydelta) ? 1 : 0;
-            }
-        }
-
-        return qtyXmas;
+    private interface XmasChecker {
+        public int checkXmas(int x, int y);
     }
 
-    private boolean isXmas(ArrayList<String> wordSearch, int x, int y, int xdelta, int ydelta) {
-        char[] target = {'X', 'M', 'A', 'S'};
-        
-        for (int i = 0; i < target.length; i++) {
-            if (y >= wordSearch.size() || y < 0 || x >= wordSearch.get(y).length() || x < 0) {
-                return false;
-            }
+    private class P1Checker implements XmasChecker {
+        private ArrayList<String> wordSearch;
 
-            if (wordSearch.get(y).charAt(x) != target[i]) {
-                return false;
-            }
-            
-            x += xdelta;
-            y += ydelta;
+        public P1Checker(ArrayList<String> wordSearch) {
+            this.wordSearch = wordSearch;
         }
 
-        return true;
+        public int checkXmas(int x, int y) {
+            int qtyXmas = 0;
+
+            for (int xdelta = -1; xdelta <= 1; xdelta++) {
+                for (int ydelta = -1; ydelta <= 1; ydelta++) {
+                    qtyXmas += isXmas(x, y, xdelta, ydelta) ? 1 : 0;
+                }
+            }
+
+            return qtyXmas;
+        }
+
+        public boolean isXmas(int x, int y, int xdelta, int ydelta) {
+            char[] target = {'X', 'M', 'A', 'S'};
+
+            for (int i = 0; i < target.length; i++) {
+                if (y >= wordSearch.size() || y < 0 || x >= wordSearch.get(y).length() || x < 0) {
+                    return false;
+                }
+
+                if (wordSearch.get(y).charAt(x) != target[i]) {
+                    return false;
+                }
+
+                x += xdelta;
+                y += ydelta;
+            }
+
+            return true;
+        }
+    }
+
+    private class P2Checker implements XmasChecker {
+        private ArrayList<String> wordSearch;
+
+        public P2Checker(ArrayList<String> wordSearch) {
+            this.wordSearch = wordSearch;
+        }
+
+        public int checkXmas(int x, int y) {
+            Set<Character> diagonal = new HashSet<>();
+
+            if (wordSearch.get(y).charAt(x) != 'A'
+                || x == 0 || x == wordSearch.get(y).length() - 1
+                || y == 0 || y == wordSearch.size() - 1) {
+
+                return 0;
+            }
+
+            diagonal.add(wordSearch.get(y - 1).charAt(x - 1));
+            diagonal.add(wordSearch.get(y + 1).charAt(x + 1));
+
+            if (!diagonal.contains('M') || !diagonal.contains('S')) {
+                return 0;
+            }
+
+            diagonal.clear();
+            diagonal.add(wordSearch.get(y + 1).charAt(x - 1));
+            diagonal.add(wordSearch.get(y - 1).charAt(x + 1));
+
+            if (!diagonal.contains('M') || !diagonal.contains('S')) {
+                return 0;
+            }
+
+            return 1;
+        }
     }
 }
