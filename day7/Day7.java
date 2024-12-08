2abd79ffcc5c852f2a653cf674781adb760b2999
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-08 17:25:08 -0500

    Refactor and add part2 solution

--- day7/Day7.java
+++ day7/Day7.java
@@ -10,12 +10,21 @@ public class Day7 extends AdventOfCodePuzzle {
 
     @Override
     public String part1(ArrayList<String> inputLines) {
+        return runPart(inputLines, true);
+    }
+
+    @Override
+    public String part2(ArrayList<String> inputLines) {
+        return runPart(inputLines, false);
+    }
+
+    private String runPart(ArrayList<String> inputLines, boolean part1) {
         long validEqnAnsSum = 0;
 
         for (String line : inputLines) {
             CalibrationEquation eqn = new CalibrationEquation(line);
             
-            if (eqn.hasSolution()) {
+            if (eqn.hasSolution(part1)) {
                 validEqnAnsSum += eqn.getAns();
             }
         }
@@ -42,20 +51,32 @@ public class Day7 extends AdventOfCodePuzzle {
             return ans;
         }
 
-        public boolean hasSolution() {
-            int totalOperatorCombos = (int) Math.pow(2, operands.size() - 1);
+        public boolean hasSolution(boolean part1) {
+            int numOperators = part1 ? 2 : 3;
+            int totalOperatorCombos = (int) Math.pow(numOperators, operands.size() - 1);
 
-            // operatorCombo determines the combination of + and * to apply
-            // each bit represents either add (bit is 0) or multiply (bit is 1)
+            // operatorCombo encodes the combination of operators to apply
+            // part1: each bit represents either add (bit is 0) or multiply (bit is 1)
+            // part2: each trinary (base 3) bit represents either add, mult, or concat (0, 1, 2 respectively)
             for (int operatorCombo = 0; operatorCombo < totalOperatorCombos; operatorCombo++) {
                 long result = operands.get(0);
 
                 for (int i = 0; i < operands.size() - 1; i++) {
-                    // check if i-th bit of operatorCombo is 0
-                    if ((operatorCombo & (long) Math.pow(2, i)) == 0) {
-                        result += operands.get(i + 1);
-                    } else {
-                        result *= operands.get(i + 1);
+                    // check i-th bit of operatorCombo for operator to use
+                    switch(accessIthBit(numOperators, operatorCombo, i)) {
+                        case 0:
+                            result += operands.get(i + 1);
+                            break;
+                        case 1:
+                            result *= operands.get(i + 1);
+                            break;
+                        case 2:
+                            result = Long.decode(Long.toString(result) + Long.toString(operands.get(i + 1)));
+                            break;
+                        default:
+                            throw new UnsupportedOperationException(
+                                "Number of operators exceeded number of available operators"
+                            );
                     }
                 }
 
@@ -65,6 +86,15 @@ public class Day7 extends AdventOfCodePuzzle {
             }
 
             return false;
+        }
+
+        // access i-th bit of x in the given base
+        private static int accessIthBit(int base, int x, int i) {
+            int upperBits = x % (int) Math.pow(base, i + 1);
+            int lowerBits = x % (int) Math.pow(base, i);
+            int bitToAccess = upperBits - lowerBits;
+
+            return (int) (bitToAccess / Math.pow(base, i));
         }
     }
 }
