b0368ee8e98e4ec13385c145077470567e349bd5
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-07 20:18:03 -0500

    Initial commit

---
+++ day4/Day4.java
@@ -0,0 +1,14 @@
+package aoc2024.day4;
+
+import aoc2024.AdventOfCodePuzzle;
+
+public class Day4 extends AdventOfCodePuzzle {
+    public Day4() {
+        this.day = 4;
+    }
+
+    @Override
+    public String part1(ArrayList<String> inputLines) {
+        return "";
+    }
+}
