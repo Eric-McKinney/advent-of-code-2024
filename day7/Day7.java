76718ca4abe30c38d517e104b038d32bde501ef7
Eric-McKinney <ericmckinney.md@gmail.com>
2024-12-08 16:22:03 -0500

    Add part1 solution

--- day7/Day7.java
+++ day7/Day7.java
@@ -10,6 +10,61 @@ public class Day7 extends AdventOfCodePuzzle {
 
     @Override
     public String part1(ArrayList<String> inputLines) {
-        return "";
+        long validEqnAnsSum = 0;
+
+        for (String line : inputLines) {
+            CalibrationEquation eqn = new CalibrationEquation(line);
+            
+            if (eqn.hasSolution()) {
+                validEqnAnsSum += eqn.getAns();
+            }
+        }
+
+        return Long.toString(validEqnAnsSum);
+    }
+
+    private class CalibrationEquation {
+        private long ans;
+        private ArrayList<Long> operands;
+
+        public CalibrationEquation(String eq) {
+            String[] nums = eq.split(":? +");
+            this.operands = new ArrayList<>();
+
+            this.ans = Long.decode(nums[0]);
+            
+            for (int i = 1; i < nums.length; i++) {
+                operands.add(Long.decode(nums[i]));
+            }
+        }
+
+        public long getAns() {
+            return ans;
+        }
+
+        public boolean hasSolution() {
+            int totalOperatorCombos = (int) Math.pow(2, operands.size() - 1);
+
+            // operatorCombo determines the combination of + and * to apply
+            // each bit represents either add (bit is 0) or multiply (bit is 1)
+            for (int operatorCombo = 0; operatorCombo < totalOperatorCombos; operatorCombo++) {
+                long result = operands.get(0);
+
+                for (int i = 0; i < operands.size() - 1; i++) {
+                    // check if i-th bit of operatorCombo is 0
+                    if ((operatorCombo & (long) Math.pow(2, i)) == 0) {
+                        result += operands.get(i + 1);
+                    } else {
+                        result *= operands.get(i + 1);
+                    }
+                }
+
+                if (result == ans) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
